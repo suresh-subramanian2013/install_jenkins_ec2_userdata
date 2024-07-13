ca7bacc8722449ff553b9d6191f28191b4fd1e34
Suresh Kumar S <98449018+suresh-subramanian2013@users.noreply.github.com>
2024-07-13 06:15:26 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo yum update –y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
-https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm — import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade -y
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
 sudo dnf install java-17-amazon-corretto -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
